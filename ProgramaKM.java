7ff7d601bb41c1300e8436a7a6870c83fd00073c
jorge <jsobrino98@gmail.com>
2024-02-28 11:48:00 +0100

    Conversion de km a metros

---
+++ ProgramaKM.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ProgramaKM {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int num, numFinal;
+
+        System.out.println("Escribe el numero en Km");
+        num = teclado.nextInt();
+
+        numFinal=num*1000;
+
+        System.out.println("La distancia en metros es: " + numFinal);
+    }
+}
