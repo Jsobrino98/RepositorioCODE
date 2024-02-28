ed1676f3d863e097ed29b9315b8cc041f675cd38
Jsobrino98 <159003886+Jsobrino98@users.noreply.github.com>
2024-02-28 11:59:29 +0100

    ProgramaCompleto

--- ProgramaKM.java
+++ ProgramaKM.java
@@ -5,14 +5,12 @@ public class ProgramaKM {
         Scanner teclado = new Scanner(System.in);
 
         int num, numFinal;
-
+    //Solicitamos al usuario que introduzca el numero de Km que quiere saber en metros
         System.out.println("Escribe el numero en Km");
         num = teclado.nextInt();
-
+    //Multiplicamos el numero introducido por teclado por 1000
         numFinal=num*1000;
-
+    //Imprimimos un mensaje con la solucion final
         System.out.println("La distancia en metros es: " + numFinal);
-
-        //Joje dice paco que me comas los KM
     }
 }
